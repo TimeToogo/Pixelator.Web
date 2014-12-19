45ea4aa3c689f9bcd6e8f5569bce8ad6f789dbc5
Elliot Levin <elliot@aanet.com.au>
2014-12-19 23:32:31 +1100

    Add custom error pages

---
+++ Pixelator.Web/Controllers/ErrorController.cs
@@ -0,0 +1,23 @@
+﻿using System.Web.Mvc;
+
+namespace Pixelator.Web.Controllers
+{
+    public class ErrorController : BaseController
+    {
+        //
+        // GET: /Error/
+
+        public ViewResult Index()
+        {
+            return AppropriateView();
+        }
+
+        //
+        // GET: /Error/NotFound/
+
+        public ViewResult NotFound()
+        {
+            return AppropriateView();
+        }
+    }
+}
