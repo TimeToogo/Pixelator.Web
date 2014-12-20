ddb7338c84ae297d731070db0593f367148c4af4
Elliot Levin <elliot@aanet.com.au>
2014-12-20 20:47:37 +1100

    Added error logging

--- Pixelator.Web/Global.asax.cs
+++ Pixelator.Web/Global.asax.cs
@@ -1,4 +1,7 @@
-﻿using System.Web.Http;
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
@@ -14,6 +17,34 @@ namespace Pixelator.Web
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+        
+        protected void Application_Error(object sender, EventArgs e)
+        {
+            Exception exception = Server.GetLastError(); 
+            StringBuilder builder = new StringBuilder();
+
+            builder
+                .AppendLine("----------")
+                .AppendLine(DateTime.Now.ToString())
+                .AppendFormat("Source:\t{0}", exception.Source)
+                .AppendLine()
+                .AppendFormat("Target:\t{0}", exception.TargetSite)
+                .AppendLine()
+                .AppendFormat("Type:\t{0}", exception.GetType().Name)
+                .AppendLine()
+                .AppendFormat("Message:\t{0}", exception.Message)
+                .AppendLine()
+                .AppendFormat("Stack:\t{0}", exception.StackTrace)
+                .AppendLine();
+
+            string filePath = Server.MapPath("~/App_Data/Error.log");
+
+            using (StreamWriter writer = File.AppendText(filePath))
+            {
+                writer.Write(builder.ToString());
+                writer.Flush();
+            }
         }
     }
 }
